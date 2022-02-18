5161712b9319a52cf0b2c6cd3108503967032a9a
Arnulfo Mendieta <mendieta.arnulfo@gmail.com>
2022-02-18 01:28:54 -0500

    Added some changes to HEADERS request endpoint

--- src/App.js
+++ src/App.js
@@ -4,46 +4,46 @@ import Products from './components/Products';
 import './App.css';
 
 function App() {
-  const API_ENDPOINT = 'https://ar-holdings-dev-test.myshopify.com/api/2022-01/graphql.json';
+    const API_ENDPOINT = 'https://ar-holdings-dev-test.myshopify.com/api/2022-01/graphql.json';
 
-  const HEADERS_REQUEST = new Headers({
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-    'X-Shopify-Storefront-Access-Token': process.env.REACT_APP_STORE_FRONT_TOKEN,
-  })
-
-  const [products, setProducts] = useState([]);
-
-  const fetchShopifyStoreProducts = async (API_ENDPOINT) => {
-    await fetch(API_ENDPOINT, {
-      method: 'POST',
-      headers: HEADERS_REQUEST,
-      body: JSON.stringify({ query: queryProducts() }),
+    const HEADERS_REQUEST = new Headers({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'X-Shopify-Storefront-Access-Token': process.env.REACT_APP_STORE_FRONT_TOKEN,
     })
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result.data.products);
-        setProducts(result.data.products.edges);
-      })
-      .catch((error) => console.log(error))
-  }
 
-  useEffect(() => {
-    fetchShopifyStoreProducts(API_ENDPOINT)
-  }, []);
+    const [products, setProducts] = useState([]);
 
-  /**
-   *  Call Syncronize Method
-   */
+    const fetchShopifyStoreProducts = async(API_ENDPOINT) => {
+        await fetch(API_ENDPOINT, {
+                method: 'POST',
+                headers: HEADERS_REQUEST,
+                body: JSON.stringify({ query: queryProducts() }),
+            })
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result.data.products);
+                setProducts(result.data.products.edges);
+            })
+            .catch((error) => console.log(error))
+    }
 
-  const syncProducts = async() => {
-    await fetchShopifyStoreProducts(API_ENDPOINT);
-  }
+    useEffect(() => {
+        fetchShopifyStoreProducts(API_ENDPOINT);
+    }, []);
 
-  /**
-   * The graphql query
-   **/
-  const queryProducts = () => `
+    /**
+     *  Call Syncronize Method
+     */
+
+    const syncProducts = async() => {
+        await fetchShopifyStoreProducts(API_ENDPOINT);
+    }
+
+    /**
+     * The graphql query
+     **/
+    const queryProducts = () => `
   {
       products(first: 10) {
         edges {
@@ -75,18 +75,20 @@ function App() {
     }
   `
 
-  return (
-    <>
-      <div className="App">
-        <Header
-          titleHeader="AR Holding Products"
-          syncLabel="Sync Products"
-          syncProducts={syncProducts}
+    return ( <
+        >
+        <
+        div className = "App" >
+        <
+        Header titleHeader = "AR Holding Products"
+        syncLabel = "Sync Products"
+        syncProducts = { syncProducts }
+        /> <
+        Products products = { products }
+        /> < /
+        div > <
         />
-        <Products products={products} />
-      </div>
-    </>
-  )
+    )
 }
 
 export default App
